5a1060fdf2871d88502c74c909c763076d8a83bb
Sergey Khomushin <sergey@placer.io>
2018-12-26 17:16:09 +0200

    1.0.0

---
+++ README.md
@@ -0,0 +1,86 @@
+mailgun-nodemailer-transport
+============================
+
+## Intro
+The custom transport plugin that allows to send email using Nodemailer via [Mailgun](https://mailgun.com/)
+
+## Why?
+Created for the new v3 Mailgun API and Nodemailer v4. The plugin is very small, optimized and written in TypeScript
+
+## Support the project
+If you like to use this module please click the star button - it is very motivating.
+
+## Quick Start
+Install mailgun-nodemailer-transport using [npm](https://www.npmjs.com/):
+
+``` bash
+$ npm install mailgun-nodemailer-transport --save
+```
+
+## Documentation
+[Nodemailer](https://nodemailer.com/message/#commmon-fields) common fields are supported and replyTo
+
+## Examples
+
+__send simple email__
+``` js
+  'use strict';
+  const nodemailer = require('nodemailer');
+  const MailgunTransport = require('mailgun-nodemailer-transport');
+
+  let transporter = nodemailer.createTransport(new MailgunTransport({
+    auth: {
+      domain: 'your domain name https://app.mailgun.com/app/domains',
+      apiKey: '12124124124124-key-test'
+    }
+  }));
+
+  transporter.sendMail({
+    from: 'email@example.com',
+    to: 'recipient@test.com',
+    replyTo: 'reply-to@example.com',
+    subject: 'Mailgun Transport',
+    text: 'This is text content'
+  }).then((info) => {
+    console.log('SUCCESS');
+  }).catch((error) => {
+    console.log('Something is wrong');
+  });
+```
+
+__send attachment and add to content__
+``` js
+  'use strict';
+  const nodemailer = require('nodemailer');
+  const MailgunTransport = require('mailgun-nodemailer-transport');
+
+  let transporter = nodemailer.createTransport(new MailgunTransport({
+    auth: {
+      domain: 'your domain name https://app.mailgun.com/app/domains',
+      apiKey: '12124124124124-key-test'
+    }
+  }));
+
+  transporter.sendMail({
+    from: 'email@example.com',
+    to: 'recipient@test.com',
+    replyTo: 'reply-to@example.com',
+    subject: 'Mailgun Transport',
+    html: '<!DOCTYPE html><html><body><img src="cid:attachment" alt="attachment"></body></html>',
+    attachments: [{
+      content: '/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAA...', // base64 content
+      cid: 'attachment',
+      contentType: 'image/jpeg',
+      filename: 'attachment.jpg',
+      encoding: 'base64'
+    }]
+  }).then((info) => {
+    console.log('SUCCESS');
+  }).catch((error) => {
+    console.log('Something is wrong');
+  });
+```
+
+## License
+
+[MIT](./LICENSE)
---
+++ src/MailgunTransport.ts
@@ -0,0 +1,125 @@
+import {Transport, SendMailOptions} from 'nodemailer';
+import * as FormData from 'form-data';
+
+const TRANSFORM_FIELDS: Object = {
+  replyTo: 'h:Reply-To'
+};
+
+const ADDRESS_KEYS: Array<string> = ['from', 'to', 'cc', 'bcc', 'replyTo'];
+const CONTENT_KEYS: Array<string> = ['subject', 'text', 'html'];
+
+export interface Options {
+  auth: {
+    domain: string;
+    apiKey: string
+  };
+}
+
+export class MailgunTransport implements Transport {
+
+  private requestConfig: FormData.SubmitOptions;
+
+  public name: string = 'MailgunTransport';
+  public version: string = 'N/A';
+
+  constructor(options: Options) {
+    this.requestConfig = {
+      protocol: 'http:',
+      hostname: `api.mailgun.net`,
+      path: `/v3/${options.auth.domain}/messages`,
+      auth: `api:${options.auth.apiKey}`
+    };
+  }
+
+  private combineTarget(target: {name: string; address: string}): string {
+    return target.name ? `${target.name} <${target.address}>` : target.address;
+  }
+
+  private appendAddresses(form: FormData, data: SendMailOptions): void {
+    ADDRESS_KEYS.forEach(target => {
+      if (!data[target]) return;
+
+      let value: string;
+
+      if (Array.isArray(data[target])) {
+        value = data[target].map(this.combineTarget).join(',');
+      } else {
+        value = this.combineTarget(data[target]);
+      }
+
+      form.append(TRANSFORM_FIELDS[target] || target, value);
+    });
+  }
+
+  private appendContent(form: FormData, data: SendMailOptions): void {
+    CONTENT_KEYS.forEach(content => {
+      if (!data[content]) return;
+
+      form.append(TRANSFORM_FIELDS[content] || content, data[content]);
+    });
+  }
+
+  private appendAttachments(form: FormData, data: SendMailOptions): void {
+    if (!Array.isArray(data.attachments)) return;
+
+    data.attachments.forEach((attachment) => {
+      let buffer: Buffer = Buffer.from(<string>attachment.content, attachment.encoding);
+
+      form.append('inline', buffer, {
+        filename: attachment.cid,
+        contentType: attachment.contentType,
+        knownLength: buffer.length
+      });
+    });
+  }
+
+  private submitForm(form: FormData): Promise<any> {
+    return new Promise((resolve, reject) => {
+      form.submit(this.requestConfig, (err, res) => {
+        if (err) return reject(err);
+
+        let chunks: Array<any> = [];
+
+        res.on('data', (chunk) => chunks.push(chunk));
+
+        res.on('end', () => {
+          let answer: any = Buffer.concat(chunks).toString();
+
+          if (res.statusCode === 200) {
+            resolve(answer);
+          } else {
+            reject(answer);
+          }
+        });
+
+        res.on('error', (error) => {
+          reject(error);
+        });
+      });
+    });
+  }
+
+  public send(mail: any, done: Function): void {
+    setImmediate(() => {
+      mail.normalize((error, data: SendMailOptions) => {
+        if (error) return done(error);
+
+        let form: FormData = new FormData();
+
+        this.appendAddresses(form, data);
+        this.appendContent(form, data);
+        this.appendAttachments(form, data);
+
+        this.submitForm(form)
+          .then(() => {
+            done(null, {
+              envelope: mail.message.getEnvelope(),
+              messageId: mail.message.messageId(),
+              message: form
+            });
+          })
+          .catch((e) => done(e));
+      });
+    });
+  }
+}
